6caacd7ae0673074a3dc8d139d8fbfc5b929fcb2
PASPARTU <DanWorkMail@yandex.ru>
2019-04-28 13:12:49 +0400

    1 лаба по Дауровой
    Трассировка маршрута

---
+++ Daurova/Трассировка маршрута/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace трассировка_маршрута
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Location = new System.Drawing.Point(12, 111);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "20";
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(48, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "20";
+            this.textBox2.Visible = false;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(104, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Свободная клетка";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(184, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Стенка";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(264, 34);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 36);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Стартовая точка";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DarkViolet;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(344, 34);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Конечная точка";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LimeGreen;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(424, 34);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 36);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Найти маршрут";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(626, 724);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Daurova/Трассировка маршрута/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace трассировка_маршрута
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        bool flag = false;
+
+        int setButonState = 0;
+
+        int n;
+        int m;
+
+        Button[,] buton;
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(textBox1.Text);
+            m = Convert.ToInt32(textBox2.Text);
+
+            GenerateTable(n, m);
+        }
+
+        void GenerateTable(int n, int m)
+        {
+            buton = new Button[n, m];
+
+            int BtnWidth = 30;
+            int BtnHeight = 30;
+            int IndentLeft = 0;
+            int IndentTop = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+
+                    buton[i, j] = new Button();
+                    buton[i, j].Parent = panel1;
+                    buton[i, j].Left = (BtnWidth * j) + (IndentLeft * j);
+                    buton[i, j].Top = (BtnHeight * i) + (IndentTop * i);
+                    buton[i, j].Width = BtnWidth;
+                    buton[i, j].Height = BtnHeight;
+
+                    buton[i, j].BackColor = Color.MediumTurquoise;
+                    buton[i, j].FlatStyle = FlatStyle.Flat;
+
+                    buton[i, j].MouseClick += SetFlag;
+                    buton[i, j].MouseMove += Zakraska;
+
+
+                }
+        }
+
+        void DisposeTable(int n, int m)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    buton[i, j].Dispose();
+                }
+        }
+
+        void SetFlag(object sender, EventArgs e)
+        {
+            if (flag) { flag = false; }
+            else { flag = true; }
+
+            if (setButonState == 2)
+            {
+                FoundSingleButonColor(Color.MediumSlateBlue);
+                (sender as Button).BackColor = Color.MediumSlateBlue;
+            }
+
+            if (setButonState == 3)
+            {
+                FoundSingleButonColor(Color.DarkViolet);
+                (sender as Button).BackColor = Color.DarkViolet;
+            }
+
+        }
+
+        void Zakraska(object sender, EventArgs e)
+        {
+
+            if (setButonState == 0 && flag)
+            {
+                (sender as Button).BackColor = Color.MediumTurquoise;
+            }
+            if (setButonState == 1 && flag)
+            {
+                (sender as Button).BackColor = Color.IndianRed;
+            }
+        }
+
+        void FoundSingleButonColor(Color c)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    if (buton[i, j].BackColor == c)
+                    {
+                        buton[i, j].BackColor = Color.MediumTurquoise;
+                        break;
+                    }
+                }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DisposeTable(n, m);
+
+                n = Convert.ToInt32(textBox1.Text);
+                m = Convert.ToInt32(textBox2.Text);
+
+                GenerateTable(n, m);
+            }
+            catch
+            { }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                DisposeTable(n, m);
+
+                n = Convert.ToInt32(textBox1.Text);
+                m = Convert.ToInt32(textBox2.Text);
+
+                GenerateTable(n, m);
+            }
+            catch
+            { }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            setButonState = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            setButonState = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            setButonState = 2;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            setButonState = 3;
+        }
+
+
+        //========================================
+        // Расчетная логика
+
+
+
+
+        int[,] table;
+        int[,] tableWay;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // очистка поля от значений
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    buton[i, j].Text = "";
+                    if (buton[i, j].BackColor == Color.LimeGreen)
+                    { buton[i, j].BackColor = Color.MediumTurquoise; }
+                }
+
+            //
+            table = new int[n + 2, m + 2];
+            tableWay = new int[n, m];
+            for (int i = 0; i < n + 2; i++)
+                for (int j = 0; j < m + 2; j++)
+                { table[i, j] = 1; }
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    tableWay[i, j] = 0;
+
+                    if (buton[i, j].BackColor == Color.MediumTurquoise || buton[i, j].BackColor == Color.LimeGreen)
+                    { table[i + 1, j + 1] = 0; }
+                    if (buton[i, j].BackColor == Color.IndianRed)
+                    { table[i + 1, j + 1] = 1; }
+                    if (buton[i, j].BackColor == Color.MediumSlateBlue)
+                    {
+                        table[i + 1, j + 1] = 2;
+                        tableWay[i, j] = 1;
+                    }
+                    if (buton[i, j].BackColor == Color.DarkViolet)
+                    {
+                        table[i + 1, j + 1] = 3;
+                    }
+                }
+
+            Calculate();
+        }
+
+
+
+        void Calculate()
+        {
+            int wayWeight = 1;
+
+            // если 0 то не все поле пройденно
+            // если 1 то найденна конечная точка
+            // если 2 то конечная точка не достижима
+            int stop = 0;
+
+            while (stop == 0)
+            //for (int k = 0; k < 5; k++)
+            {
+                bool vhodVUslovie = false;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (tableWay[i, j] == wayWeight)
+                        {
+                            // проход по свободным точкам
+                            if (table[i + 1 + 1, j + 1] != 1 && tableWay[i + 1, j] == 0)
+                            { tableWay[i + 1, j] = wayWeight + 1; }
+                            if (table[i + 1, j + 1 + 1] != 1 && tableWay[i, j + 1] == 0)
+                            { tableWay[i, j + 1] = wayWeight + 1; }
+                            if (table[i + 1 - 1, j + 1] != 1 && tableWay[i - 1, j] == 0)
+                            { tableWay[i - 1, j] = wayWeight + 1; }
+                            if (table[i + 1, j + 1 - 1] != 1 && tableWay[i, j - 1] == 0)
+                            { tableWay[i, j - 1] = wayWeight + 1; }
+
+                            // регисрация конечной точки
+                            if (table[i + 1 + 1, j + 1] == 3)
+                            { stop = 1; }
+                            if (table[i + 1, j + 1 + 1] == 3)
+                            { stop = 1; }
+                            if (table[i + 1 - 1, j + 1] == 3)
+                            { stop = 1; }
+                            if (table[i + 1, j + 1 - 1] == 3)
+                            { stop = 1; }
+
+                            vhodVUslovie = true;
+                        }
+                    }
+                }
+                if (!vhodVUslovie)
+                {
+                    stop = 2;
+                }
+                wayWeight++;
+            }
+
+
+            OutTableWayOnButons(tableWay);
+
+            if (stop == 1)
+            {
+                int setX = 1;
+                int setY = 1;
+                int setWeight = 0;
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                    {
+                        if (table[i + 1, j + 1] == 3)
+                        {
+                            setX = i;
+                            setY = j;
+                            setWeight = tableWay[i, j];
+                        }
+                    }
+
+                for (int k = setWeight; k > 1 + 1; k--)
+                {
+                    if (table[setX + 1 + 1, setY + 1] != 1 && tableWay[setX + 1, setY] == k - 1)
+                    { buton[setX + 1, setY].BackColor = Color.LimeGreen; setX = setX + 1; }
+                    if (table[setX + 1, setY + 1 + 1] != 1 && tableWay[setX, setY + 1] == k - 1)
+                    { buton[setX, setY + 1].BackColor = Color.LimeGreen; setY = setY + 1; }
+                    if (table[setX + 1 - 1, setY + 1] != 1 && tableWay[setX - 1, setY] == k - 1)
+                    { buton[setX - 1, setY].BackColor = Color.LimeGreen; setX = setX - 1; }
+                    if (table[setX + 1, setY + 1 - 1] != 1 && tableWay[setX, setY - 1] == k - 1)
+                    { buton[setX, setY - 1].BackColor = Color.LimeGreen; setY = setY - 1; }
+                }
+            }
+
+        }
+
+        void OutTableWayOnButons(int[,] tbl)
+        {
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                {
+                    if (tbl[i, j] != 0)
+                    {
+                        buton[i, j].Text = tbl[i, j].ToString();
+                    }
+                }
+        }
+    }
+}
